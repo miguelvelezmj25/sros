630196a7d984d310b531ed5b47df782d76d47412
Miguel Velez <miguelvelez@mijecu25.com>
2019-09-03 09:48:03 -0400

    added echo messages for logging the testing of running nodes

--- scripts/test_running_nodes.sh
+++ scripts/test_running_nodes.sh
@@ -2,6 +2,11 @@
 
 # Execute scripts to test the nodes that are running
 
+echo "Running the ROS2 node that publishes messages"
 ./run_publisher.sh &
+echo "Waiting 2 seconds for the above node to start publishing messages"
 sleep 2
+echo "Running the script to check the running nodes"
 ./monitor_running_nodes.sh &
+sleep 10
+echo "Done testing"
