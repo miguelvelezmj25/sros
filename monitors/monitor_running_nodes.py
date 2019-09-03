13d68fd1e3bbe9e6ac8ea46d01af8ab4c22b0320
Miguel Velez <miguelvelez@mijecu25.com>
2019-09-03 09:16:27 -0400

    initial commit

---
+++ monitors/monitor_running_nodes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Simple script to check the list of running ROS2 nodes. Raises an AssertionError if any nodes are listed.
+#
+# The script is currently used to check configuring the security of ROS2 using SROS. We observed that despite configuring 
+# the environement correctly, the name of "securely" running nodes can be retrieved from the running system and an attacker
+# could initialize a node with one of the names listed in the results. Hence, this script should ALWAYS raise an exception
+# even if the environment was configured to run with security enabled.
+#
+# Maybe, a better check would be to retrieve the list of running nodes and check if the ones that are running securely are
+# not listed and the ones that are lists were not configured securely, either on purpose or by mistake.
+
+import subprocess
+
+if __name__ == '__main__':
+  output = subprocess.check_output(["ros2", "node", "list"])
+
+  if output:
+    raise AssertionError("Should not be able to see any nodes running. We saw the following nodes running:\n" + output)
+
+  print 'OK. Did not see any nodes running'
